480ed950bca3c02f2e5f36878a66771f8b810835
Lê Hoàng Huy <ducv.nguyen@mission.plus>
2025-01-04 20:38:53 +0700

    chore: Finish GR1 project.

--- api/app.js
+++ api/app.js
@@ -1,19 +1,38 @@
 import express from "express";
+import cors from "cors";
 import cookieParser from "cookie-parser";
 import authRoute from "./routes/auth.route.js";
-import postRoute from "./routes/post.route.js";
+import postRoute from "./routes/post.route.js"; //
+import testRoute from "./routes/test.route.js";
+import userRoute from "./routes/user.route.js";
+import chatRoute from "./routes/chat.route.js";
+import messageRoute from "./routes/message.route.js";
+
 const app = express();
 
-// Middleware xử lý JSON và URL-encoded (không cần thiết trong ví dụ này, nhưng hữu ích cho các API thực tế)
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
-app.use(express.urlencoded({ extended: true }));
-//heavy is the crown
 
-app.use(express.json());
-app.use("/api/posts",postRoute);
-app.use("/api/auth",authRoute);
-// Lắng nghe trên cổng 8800
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/posts", postRoute);
+app.use("/api/test", testRoute);
+app.use("/api/chats", chatRoute);
+app.use("/api/messages", messageRoute);
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*'); // Cho phép tất cả các nguồn
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Credentials', 'true'); // Nếu cần dùng cookie
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end(); // Kết thúc preflight request
+  }
+  next();
+});
+
+
+
 app.listen(8800, () => {
-  console.log("Server is running ");
+  console.log("Server is running!");
 });
